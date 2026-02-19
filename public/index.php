50b39b75fef32858f7934cf3f336c64bf8d40344
Hind AlQamshoui <hindalqamshoui@iMac.local>
2026-02-19 10:38:22 +0400

    Debug: Enable error reporting to diagnose 500 error

--- public/index.php
+++ public/index.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Autoload Controllers (Manual for now)
 require_once __DIR__ . '/../src/config.php';
 require_once __DIR__ . '/../src/helpers.php';
